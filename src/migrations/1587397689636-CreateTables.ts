a2caf1649bc660080d00341b6c103c5e9c50c598
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2020-04-20 18:00:08 +0200

    update dependencies

--- src/migrations/1587397689636-CreateTables.ts
+++ src/migrations/1587397689636-CreateTables.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateTables1587046310095 implements MigrationInterface {
-    name = 'CreateTables1587046310095'
+export class CreateTables1587397689636 implements MigrationInterface {
+    name = 'CreateTables1587397689636'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "uuid" uuid NOT NULL DEFAULT uuid_generate_v4(), "first_name" character varying NOT NULL, "last_name" character varying NOT NULL, "email" character varying NOT NULL, "phone" character varying, "avatar" character varying, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`, undefined);
