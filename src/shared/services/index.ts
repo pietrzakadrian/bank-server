ffa65da450412a3a211507daaec083d536971543
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2020-06-16 19:38:06 +0200

    update transaction controller

--- src/shared/services/index.ts
+++ src/shared/services/index.ts
@@ -2,5 +2,4 @@ export * from './aws-s3.service';
 export * from './config.service';
 export * from './generator.service';
 export * from './validator.service';
-export * from './settings.service';
 export * from './app.service';
