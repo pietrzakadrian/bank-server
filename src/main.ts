8ac853f60ba00ab73ac13aebdce22a56e7615533
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2020-06-20 15:18:39 +0200

    add swagger module, cleancode

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as compression from 'compression';
+import { AppModule } from './modules/app/app.module';
+import { setupSwagger } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(compression());
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
