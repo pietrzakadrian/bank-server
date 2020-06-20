604650f2a5472ecdd4de484c925dfbe5c9f97049
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2020-06-20 14:58:49 +0200

    add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '51.38.132.176',
+      port: 5432,
+      username: 'admin_banknew22',
+      password: 'admin_banknew22',
+      database: 'admin_banknew22',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
